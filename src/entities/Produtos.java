ed16621a32a940fe181f3b69a1d29a071d36ed75
AnaLuizaEliasGama <analuizagama89@gmail.com>
2025-04-23 17:02:37 -0300

    Adicao de constructor e getter  setter

--- src/entities/Produtos.java
+++ src/entities/Produtos.java
@@ -1,5 +1,48 @@
 package entities;
 
 public class Produtos {
+    private String  nome;
+    private String  descricao;
+    private double preco;
+    private int quantidade;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produtos(String nome, String descricao, double preco, int quantidade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+
+    }
 }
